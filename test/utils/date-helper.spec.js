596ee4f2b58bc9d9f6828b6701e7b557cad3c37f
Jim Buck <jim@jimmyboh.com>
2016-01-14 09:22:34 -0500

    More tests. Fixed .gitignore

--- test/utils/date-helper.spec.js
+++ test/utils/date-helper.spec.js
@@ -21,3 +21,47 @@ test('3-day week ending on friday returns start of previous wednesday', t => {
 
   t.ok(expectedStartDate.isSame(actualStartDate));
 });
+
+test('Midweek is contained within week.', t => {
+  const end = '2016-01-08';
+  const day = '2016-01-05';
+
+  const withinWeek = dateHelper.isWithinWeek(end, day);
+
+  t.ok(withinWeek);
+});
+
+test('Before is not contained within week.', t => {
+  const end = '2016-01-08';
+  const day = '2015-12-25';
+
+  const withinWeek = dateHelper.isWithinWeek(end, day);
+
+  t.notOk(withinWeek);
+});
+
+test('After is not contained within week.', t => {
+  const end = '2016-01-08';
+  const day = '2016-01-10';
+
+  const withinWeek = dateHelper.isWithinWeek(end, day);
+
+  t.notOk(withinWeek);
+});
+
+test('Week end is contained within week.', t => {
+  const end = '2016-01-08';
+
+  const withinWeek = dateHelper.isWithinWeek(end, end);
+
+  t.ok(withinWeek);
+});
+
+test('Week start is contained within week.', t => {
+  const end = '2016-01-08';
+  const day = '2016-01-02';
+
+  const withinWeek = dateHelper.isWithinWeek(end, day);
+
+  t.ok(withinWeek);
+});
