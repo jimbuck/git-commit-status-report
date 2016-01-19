69d570a80b144b448615e31034bc4686c610c399
Jim Buck <jim@jimmyboh.com>
2016-01-18 23:18:12 -0500

    Implemented update-notifier.

--- bin/status.js
+++ bin/status.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node --use_strict
 
 const vorpal = require('vorpal')();
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+updateNotifier({ pkg });
 
 vorpal
   .command('foo', 'Outputs "bar"')
